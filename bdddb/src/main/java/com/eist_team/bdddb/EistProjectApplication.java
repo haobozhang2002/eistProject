478ab7b07b8f5d557dfc90d855faeddc653f5489
haobozhang2002 <108615068+haobozhang2002@users.noreply.github.com>
2022-07-03 15:00:00 +0200

    Add files via upload

---
+++ bdddb/src/main/java/com/eist_team/bdddb/EistProjectApplication.java
@@ -0,0 +1,13 @@
+package com.eist_team.bdddb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EistProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EistProjectApplication.class, args);
+	}
+
+}
---
+++ bdddb/src/test/java/com/eist_team/bdddb/EistProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eist_team.bdddb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EistProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
